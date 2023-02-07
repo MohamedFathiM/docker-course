8833335cb07767d34ed0d0c481cb74769022890a
Mohamed <mohamedkdr66@yahoo.com>
2023-02-07 08:46:49 +0200

    move to src folder

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const PORT = 4000;
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1> Hello From Node js , hi</h1>"));
+app.get("/", (req, res) =>
+  res.send("<h1> Hello From Node js ,Hello World , hi</h1>")
+);
 app.listen(PORT, () => console.log(`App is Running on : ${PORT}`));
