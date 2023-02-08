6a59ce2013db63f120a7c74c7b40f57522f743c3
Mohamed <mohamedkdr66@yahoo.com>
2023-02-08 16:59:21 +0200

    add mongoose and connect to DB , using docker-compose to connect on multiple containers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const PORT = 4000;
 const app = express();
+const mongoose = require("mongoose");
+
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+mongoose
+  .connect(URI)
+  .then(() => console.log("Connected "))
+  .catch((err) => console.log("Failed To Connect", err));
 
 app.get("/", (req, res) =>
   res.send("<h1> Hello From Node js  World , hi</h1>")
